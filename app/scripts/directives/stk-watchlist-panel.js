59c48fdcf939c953be94b98420780ef992f7dec1
Jasmeet Singh <jascee.gtbit@gmail.com>
2017-01-26 00:35:25 +0530

    Watchlist Directive added

---
+++ app/scripts/directives/stk-watchlist-panel.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name stockMasterApp.directive:stkWatchlistPanel
+ * @description
+ * # stkWatchlistPanel
+ */
+angular.module('stockMasterApp')
+	// [1] register direct and inject dependencies
+  .directive('stkWatchlistPanel', function ($location , $modal, WatchlistService) {
+    return {
+      templateUrl: ' views/ templates/watchlist-panel.html',
+      restrict: 'E',
+      scope: {},
+      link: function($scope){
+      	 // [2] Initialize variables
+      	 $scope.watchlists = {};
+      	 var addListModal = $modal({
+      	 	scope: $scope,
+      	 	template: 'views/templates/addlist-modal.html',
+      	 	show: false
+      	 });
+
+      	 // [3] Bind model from service to this scope
+      	 $scope.watchlists = WatchlistService.query();
+
+      	 // [4] Display addlist Modal
+      	 $scope.showModal = function(){
+      	 	addListModal.$promise.then(addListModal.show);
+      	 };
+      	 //[5] Create a new list from fields in modal
+      	 $scope.createList = function() {
+      	 	WatchlistService.save($scope.watchlist);
+      	 	addListModal.hide();
+      	 	$scope.watchlist = {};
+      	 };
+      	  // [6] Delete desired list and redirect to home
+      	  $scope.deleteList = function(list){
+      	  	WatchlistService.remove(list);
+      	  	$location.path('/');
+      	  };
+      }
+    };
+  });
