ee98f7837e95c60f3059f925efb43f0511cef239
Jasmeet Singh <jascee.gtbit@gmail.com>
2017-01-25 15:45:33 +0530

    Added Functional Programming Library and watchlist service

---
+++ app/scripts/services/watchlist-service.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name stockMasterApp.WatchlistService
+ * @description
+ * # WatchlistService
+ * Service in the stockMasterApp.
+ */
+angular.module('stockMasterApp')
+  .service('WatchlistService', function WatchlistService() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
