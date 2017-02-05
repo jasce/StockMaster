76c38444840e7237fce8e5af5c2f7ba91129bdf7
Jasmeet Singh <jascee.gtbit@gmail.com>
2017-02-05 22:58:59 +0530

    Watchlist Controller Changed

--- app/scripts/controllers/watchlist.js
+++ app/scripts/controllers/watchlist.js
@@ -8,10 +8,28 @@
  * Controller of the stockMasterApp
  */
 angular.module('stockMasterApp')
-  .controller('WatchlistCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('WatchlistCtrl', function ($scope , $routeParams , $modal, WatchlistService , CompanyService) {
+   $scope.companies = CompanyService.query();
+   $scope.watchlist = WatchlistService.query($routeParams.listId);
+   $scope.stocks = $scope.watchlist.stocks;
+   $scope.newStock = {};
+   var addStockModal = $modal({
+   	scope: $scope,
+   	template: 'views/templates/addstock-modal.html',
+   	show: false
+   });
+   // Expose showStockModal to view via $scope
+   $scope.showStockModal = function(){
+   	addStockModal.$promise.then(addStockModal.show);
+   };
+   // Call the WatchlistModel addStock() function and hide the modal
+   $scope.addStock = function(){
+   	$scope.watchlist.addStock({
+   		listId: $routeParams.listId,
+   		company: $scope.newStock.company,
+   		shares: $scope.newStock.shares
+   	});
+   	addStockModal.hide();
+   	$scope.newStock = {};
+   };
   });
